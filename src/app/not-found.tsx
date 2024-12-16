8ee035097942d320d3676d09384d0ccff6eb4484
Ogame3334 <kazuhiroisobe0522@gmail.com>
2024-12-16 09:58:07 +0900

    404の時のページ追加

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// app/not-found.tsx
+export default function NotFound() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div className="text-2xl text-center">404 - Page Not Found</div>
+        <div className="my-5 text-sm text-center">お探しのページは見つかりませんでした。</div>
+      </div>
+    );
+  }
+  
